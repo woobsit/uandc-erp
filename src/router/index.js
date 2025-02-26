4b2b91e9e832f84a26eff53087a49d98c0b2b441
woobsit <woobsit@gmail.com>
2025-02-26 10:15:15 +0100

    Login feature is completed

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Dashboard from '@/views/dashboard/DashboardPage.vue';
 import ViewOrders from '@/views/dashboard/ViewOrders.vue';
 import CreateOrders from '@/views/dashboard/CreateOrders.vue';
 //import OrderHistory from '@/views/dashboard/OrderHistory.vue';
+import Cookies from 'js-cookie';
 
 const routes = [
   {
@@ -19,16 +20,19 @@ const routes = [
         path: 'home',
         name: 'Home',
         component: Dashboard,
+        meta: { requiresAuth: true },
       },
       {
         path: 'view-orders',
         name: 'Orders',
         component: ViewOrders,
+        meta: { requiresAuth: true },
       },
       {
         path: 'create-orders',
         name: 'Create orders',
         component: CreateOrders,
+        meta: { requiresAuth: true },
       },
       // {
       //   path: 'orders-history',
@@ -45,16 +49,19 @@ const routes = [
         path: 'login',
         name: 'Login',
         component: LoginPage,
+        meta: { requiresGuest: true },
       },
       {
         path: 'forget-password',
         name: 'Forget password',
         component: ForgetPassword,
+        meta: { requiresGuest: true },
       },
       {
         path: 'change-password',
         name: 'Change password',
         component: ChangePassword,
+        meta: { requiresGuest: true },
       },
     ],
   },
@@ -63,6 +70,25 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+// Global Navigation Guard
+router.beforeEach((to, from, next) => {
+  // Check if user is authenticated by reading the cookie (or from a store)
+  const isAuthenticated = !!Cookies.get('auth_admin_data');
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    // If the route requires authentication and the user is not authenticated,
+    // redirect to the login page.
+    return next({ name: 'login' });
+  } else if (to.meta.requiresGuest && isAuthenticated) {
+    // If the route is for guests only (like login) and the user is authenticated,
+    // redirect them to a protected route (e.g., home/dashboard).
+    return next({ name: 'home' });
+  } else {
+    // Otherwise, proceed as normal
+    return next();
+  }
 });
 
 export default router;
