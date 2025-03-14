330a241fa0ea77425a6e7c6e98a0a761b0262a1b
woobsit <woobsit@gmail.com>
2025-03-14 13:25:01 +0100

    Finished logout features

--- api/app/Models/Admin.php
+++ api/app/Models/Admin.php
@@ -3,12 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
    
